51fca972be51eeccbbf9d0c854ad641d016f9345
Chester <chesterdeveloper2000@gmail.com>
2020-12-13 17:24:17 +0800

    installed font-awesome

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Player from "./components/Player";
+import Song from "./components/Song"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yeahh</h1>
+      <Song/>
+      <Player />
+    </div>
+  );
+}
+
+export default App;
