d476ec10eee6ffc153d3ba743a819baf9a77bbb0
Chester <chesterdeveloper2000@gmail.com>
2020-12-13 18:47:23 +0800

    initial styling of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import "./styles/app.scss"
 import Player from "./components/Player";
 import Song from "./components/Song"
 
 function App() {
   return (
     <div className="App">
-      <h1>Yeahh</h1>
       <Song/>
       <Player />
     </div>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+
 const Player = () => {
   return (
     <div className="player">
@@ -7,7 +14,9 @@ const Player = () => {
         <p>end time</p>
       </div>
       <div className="play-control">
-          
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
       </div>
     </div>
   );
