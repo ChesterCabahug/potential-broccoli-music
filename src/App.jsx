beead3baff23054253145df3770c44e4c0ff3e24
Chester <chesterdeveloper2000@gmail.com>
2020-12-14 20:35:00 +0800

    added music to util.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Song from "./components/Song"
 function App() {
   return (
     <div className="App">
+    <audio src="https://www.dropbox.com/s/pfanatfpiwld32q/AURORA%20-%20Daydreamer%20%28Music%20Video%20%26%29.mp3?dl=0"></audio>
       <Song/>
       <Player />
     </div>
---
+++ src/util.js
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Daydreamer",
+      cover: "https://i.scdn.co/image/ab67616d00001e02ddc43da988dc80fac12c53ac",
+      artist: "AURORA",
+      audio:
+        "https://ucb652aeeb3ef4ef7315971ceb47.dl.dropboxusercontent.com/cd/0/get/BFCFyNJ7qH_06vamvtQESK9VvBByb3Gy6CI1nx7DB_HOjOwmURn3HBOtp8jINa3V3QlOy_34WVt7q_8cYvAQ1Gt8EWYLn2A9Wic8oSc51-bPFye11MLMQLEDQOAWQixWVb0/file?_download_id=5279999610958874011744764532634733801804815145967822363984166203532&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "So Am I",
+      cover: "https://i.scdn.co/image/ab67616d00001e029a95e89d24214b94de36ccf7",
+      artist: "Ava Max",
+      audio:
+        "https://uc5f32c31f4b177e60c13fb19cab.dl.dropboxusercontent.com/cd/0/get/BFBbVb4P65iBahtwTUK1XcrouMRj3jQW48s6fG7yEjFSrH040wDVmXDgAFMcqEJN5lkNRdMMmzx55L39ykR6YG3yfKZo28hhTt5zsECmk-RkgtbUpI9uRyfQbN2rEdYu-ow/file?_download_id=7703725618022401669629371074038290598720764141886985555879486756&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Thunderclouds (feat. Sia, Diplo, and Labrinth)",
+      cover: "https://i.scdn.co/image/ab67616d00001e023f159ae07dd556323f39f47b",
+      artist: "Sia, Diplo, Labrinth, LSD",
+      audio:
+        "https://ucf411a99538c5d77dc49aa94625.dl.dropboxusercontent.com/cd/0/get/BFCJ0rOzSUOePnQkPcqcDLH_brQDYXRk9dF3QWso_qwE-bjztwvQdeeLP5fxzsePMQFXPzhuCSUcNlP4Nw6uYaHc2DpHi7brFrBhspwnFYvkFmAyuZtjV__d5zVbDqmljls/file?_download_id=77231601271506469085620867363384174561620488037364314639169381078&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Got the Feeling",
+      cover: "https://i.scdn.co/image/ab67616d00001e020424b4126d728e688dfba08a",
+      artist: "Syn Cole, kirstin",
+      audio:
+        "https://uc1ce78aa5ebf33a6a98db069b5d.dl.dropboxusercontent.com/cd/0/get/BFDPS6VF2SfbAcEeVBFc5qrck7G1jhBnBvKTdwTaxZrubL9ro6VcGS_UTnqnc7OrieMq7T7beLvZeZ2IO4RalCqsavItK5VBdG7niwxZ6GEUQoqQUMHW9nutY4QXlW4E4p0/file?_download_id=08556328760487553585873156493859374813285451332887378686175698748&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Good Day for Dreaming",
+      cover: "https://i.scdn.co/image/ab67616d00001e02a6de26a824ef5f64569be17b",
+      artist: "Ruelle",
+      audio:
+        "https://ucb84e75265890b706e74465bd06.dl.dropboxusercontent.com/cd/0/get/BFBshiyGzR7Q4CKm2xpOfuAyk6wwu0jHKm0xpTTKZyJUgtuanyIRU-oLmze3xaiySQD1vKlwjrMtKRgpowvIEwmUw4IECwTHaF9ZCxPJUmtpMS4SkkzhyzyJir0Iscm6qA0/file?_download_id=6986369109787598802502038404907561680708932947283061343574830573&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Fly Away",
+      cover: "https://i.scdn.co/image/ab67616d00001e028fb79948870fc9314eccac4b",
+      artist: "Tones And I",
+      audio:
+        "https://uc81dbac7d925a0b2df3bff1b6ed.dl.dropboxusercontent.com/cd/0/get/BFC4w3sSRb7Ta6XXj5h1T7lDG074KPuLFkuhXBji7Ftu_TZQfTwRB0p5OojYK43yqax6eOiXB9dAsHU1VvAgGaS-g19M5-TgUwG-6bgkLa63oyo-6EfrI089YDP5BANccF4/file?_download_id=353081667068459842795705147414637326305005110586571239951188308&_notify_domain=www.dropbox.com&dl=1",
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
