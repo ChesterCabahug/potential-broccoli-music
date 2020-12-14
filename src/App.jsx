5d56919a7eecb37f8074d70b9de6852e8735dae3
Chester <chesterdeveloper2000@gmail.com>
2020-12-14 21:05:50 +0800

    learning about ref in react: it's querySelector

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ import data from "./util";
 function App() {
   // state
   const [songs, setSongs] = useState(data());
-  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
       <audio src="https://www.dropbox.com/s/pfanatfpiwld32q/AURORA%20-%20Daydreamer%20%28Music%20Video%20%26%29.mp3?dl=0"></audio>
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
