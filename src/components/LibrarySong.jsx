6152ea9741197e2cc859b1e26097f9f4ebde67a6
Chester <chesterdeveloper2000@gmail.com>
2020-12-17 21:47:17 +0800

    bug fix: vid 1>20 6:50 ish

--- src/components/LibrarySong.jsx
+++ src/components/LibrarySong.jsx
@@ -1,3 +1,4 @@
+import {playAudio} from "../util"
 const LibrarySong = ({
   song,
   songs,
@@ -26,14 +27,8 @@ const LibrarySong = ({
     });
     setSongs(newSongs)
     // check if song is playing
-    if (isPlaying) {
-      const playPromise = audioRef.current.play();
-      if (playPromise !== undefined) {
-        playPromise.then((audio) => {
-          audioRef.current.play();
-        });
-      }
-    }
+    playAudio(isPlaying, audioRef)
+    
   };
   return (
     <div
