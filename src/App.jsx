965ef77f657889076762ac6a17fa1b507428022f
Chester <chesterdeveloper2000@gmail.com>
2020-12-18 20:53:55 +0800

    if reach end of the song, it plays the next track

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,12 @@ function App() {
       animationPercentage: animation,
     });
   };
+
+  const songEndHandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    if(isPlaying) audioRef.current.play();
+  };
   return (
     <div className="App">
       <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
@@ -66,6 +72,7 @@ function App() {
         onLoadedMetadata={timeUpdateHandler}
         ref={audioRef}
         src={currentSong.audio}
+        onEnded={() => songEndHandler()}
       ></audio>
     </div>
   );
